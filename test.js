1ca5e38a779e5ba3a02a4117f327c75e9110a59c
cymaxxx Mathebula <cmathebula@gmail.com>
2024-04-16 16:08:55 +0000

    first commit

---
+++ test.js
@@ -0,0 +1,24 @@
+/*let sum = [];
+
+for (let i = 10; i >= sum.length; i--) {
+ sum -= i;
+}
+
+console.log(sum);
+
+function sumNums(num) {
+  let sum = 0;
+  for (let i = 1; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(sumNums(3)); // Outputs 6*/
+
+let sum = 0, num;
+
+  for (let i = 1; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
